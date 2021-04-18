cd3136d7afa42b9d8815619c713c96aea301c5d0
LayneHansen <layne.d.hansen@gmail.com>
2021-04-17 22:12:11 -0600

    added get and post methods to routes

--- Server/routes/routes.js
+++ Server/routes/routes.js
@@ -5,6 +5,10 @@ router
     .get((req, res) => {
         console.log('hit the get route');
         res.json({ success: true });
+    })
+    .post((req, res) => {
+        console.log();
+        res.json({ success: true })
     });
 
 module.exports = router;
--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,8 @@ const app = express()
 
 const PORT = process.env.PORT || 5000;
 
+app.use(routes);
+
 app.listen(PORT, () => {
     console.log('app running on PORT: ' + PORT);
 });
